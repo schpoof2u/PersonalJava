9a9e146ed95ebd38388bf8218d7d8109403aa4e4
schpoof2u <62222006+schpoof2u@users.noreply.github.com>
2020-09-16 21:28:06 -0400

    getting rid of text files and using the real java files with maven framework my dad suggested

--- src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
+++ src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
@@ -1,5 +1,6 @@
-public class HelloWorld
-{
+package com.udemy.java.shikhar.section3;
+
+public class Lecture1 {
     public static void main(String[] args)
     {
         System.out.println("Hello,Shikhar Verma :D");
