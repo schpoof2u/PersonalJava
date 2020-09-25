db5a66ab877d26348e538e8b33133e4ac7bea52d
schpoof2u <62222006+schpoof2u@users.noreply.github.com>
2020-09-25 18:52:19 -0400

    Section 3 lesson 5 made an int variable for my age

--- src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
+++ src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
@@ -3,6 +3,7 @@ package com.udemy.java.shikhar.section3;
 public class Lecture1 {
     public static void main(String[] args)
     {
-        System.out.println("Hello,Shikhar Verma :D");
+        int myage=14;
+        System.out.println("Hello I am Shikhar Verma and I am "+myage+" years old");
     }
 }
