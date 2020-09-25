878e2f89ab2a808f1de08c7c90d2fe0c4f46cd5e
schpoof2u <62222006+schpoof2u@users.noreply.github.com>
2020-09-25 19:47:47 -0400

    3.1 Starting expressions

--- src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
+++ src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
@@ -3,7 +3,11 @@ package com.udemy.java.shikhar.section3;
 public class Lecture1 {
     public static void main(String[] args)
     {
-        int myage=14;
-        System.out.println("Hello I am Shikhar Verma and I am "+myage+" years old");
+        int MyFirstNumber=(10+5)+(2*10);
+        int MySecondNumber=12;
+        int MyThirdNumber=6;
+        int MyFourthNumber=20;
+        int Mytotal=(MyFirstNumber+MySecondNumber)*MyThirdNumber/MyFourthNumber;
+        System.out.println(Mytotal);
     }
 }
