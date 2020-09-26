7866f10a7878e56e4be167cb07b8f94081e400f1
schpoof2u <62222006+schpoof2u@users.noreply.github.com>
2020-09-25 22:12:46 -0400

    3.6 starting expressions

--- src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
+++ src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
@@ -3,11 +3,12 @@ package com.udemy.java.shikhar.section3;
 public class Lecture1 {
     public static void main(String[] args)
     {
-        int MyFirstNumber=(10+5)+(2*10);
-        int MySecondNumber=12;
-        int MyThirdNumber=6;
-        int MyFourthNumber=20;
-        int Mytotal=(MyFirstNumber+MySecondNumber)*MyThirdNumber/MyFourthNumber;
-        System.out.println(Mytotal);
+        int Myfirstnumber=(10+5)+(2*10);
+        int Mysecondnumber=12;
+        int Mythirdnumber=Myfirstnumber*2;
+        int Mytotal=Myfirstnumber+Mysecondnumber+Mythirdnumber;
+        int Mylastone=1000-Mytotal;
+        System.out.println("Mytotal="+Mytotal);
+        System.out.println("Mylastone="+Mylastone);
     }
 }
