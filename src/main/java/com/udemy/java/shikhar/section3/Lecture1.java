10e290c46c1473ca758a8143fa87acab44b809cf
schpoof2u <62222006+schpoof2u@users.noreply.github.com>
2020-09-26 22:26:05 -0400

    3.8  byte,short, long and width learned about why byte,short, and long primative datatype and it's limits to storing data. width is how many bits a primative datatype can store

--- src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
+++ src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
@@ -3,14 +3,27 @@ package com.udemy.java.shikhar.section3;
 public class Lecture1 {
     public static void main(String[] args)
     {
-        //shows the the least amount of data int can store
-        int MinIntValue=Integer.MIN_VALUE;
-        //shows the max amount of data int can store
-        int MaxIntValue=Integer.MAX_VALUE;
-        System.out.println("the min value for an integer to contain is "+MinIntValue);
-        System.out.println("the max value for an integer to contain is "+MaxIntValue);
-        System.out.println("Busted MAX value(Overflow) "+(MaxIntValue+1));
-        System.out.println("Busted MIN value(Underflow) "+ (MinIntValue-1));
+        //shows the least amount of data byte can store
+        byte Myminbyte=Byte.MIN_VALUE;
+        //shows the max amount of data byte can store
+        byte Mymaxbyte=Byte.MAX_VALUE;
+        System.out.println("the least amount of data byte can store is "+Myminbyte);
+        System.out.println("the max amount of data byte can store is "+Mymaxbyte);
 
+        //shows the least amount of data short can store
+        short Myminshort=Short.MIN_VALUE;
+        //shows the max amount of data short can store
+        short Mymaxshort=Short.MAX_VALUE;
+        System.out.println("the least amount of data Short can store is "+Myminshort);
+        System.out.println("the max amount of data Short can store is "+Mymaxshort);
+
+        //shows the least amount of data long can store
+        long Myminlong=Long.MIN_VALUE;
+        //shows the max amount of data long can store
+        long Mymaxlong=Long.MAX_VALUE;
+        System.out.println("the least amount of data Short can store is "+Myminlong);
+        System.out.println("the max amount of data Short can store is "+Mymaxlong);
+        long mylong=12l;
+        System.out.println("mylong equals "+mylong+" and the value ends in a lowercase l");
     }
 }
