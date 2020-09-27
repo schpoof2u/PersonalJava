33a329c657c470f6160b549e636982862970b427
schpoof2u <62222006+schpoof2u@users.noreply.github.com>
2020-09-26 21:29:29 -0400

    3.7  primative types learned about why int is a primative datatype and it's limits to storing data

--- src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
+++ src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
@@ -3,12 +3,12 @@ package com.udemy.java.shikhar.section3;
 public class Lecture1 {
     public static void main(String[] args)
     {
-        int Myfirstnumber=(10+5)+(2*10);
-        int Mysecondnumber=12;
-        int Mythirdnumber=Myfirstnumber*2;
-        int Mytotal=Myfirstnumber+Mysecondnumber+Mythirdnumber;
-        int Mylastone=1000-Mytotal;
-        System.out.println("Mytotal="+Mytotal);
-        System.out.println("Mylastone="+Mylastone);
+        int MinIntValue=Integer.MIN_VALUE;
+        int MaxIntValue=Integer.MAX_VALUE;
+        System.out.println("the min value for an integer to contain is "+MinIntValue);
+        System.out.println("the max value for an integer to contain is "+MaxIntValue);
+        System.out.println("Busted MAX value(Overflow) "+(MaxIntValue+1));
+        System.out.println("Busted MIN value(Underflow) "+ (MinIntValue-1));
+
     }
 }
