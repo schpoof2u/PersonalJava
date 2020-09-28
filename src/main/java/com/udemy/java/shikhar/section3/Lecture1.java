106e1f6186c3ce0a118c6fcdeb819c5d3d84578e
schpoof2u <62222006+schpoof2u@users.noreply.github.com>
2020-09-28 13:05:43 -0400

    3.11  float and double primative types learned about the float and double datatypes

--- src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
+++ src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
@@ -3,11 +3,22 @@ package com.udemy.java.shikhar.section3;
 public class Lecture1 {
     public static void main(String[] args)
     {
-        //primative types challenge
-        byte mybyte=127;
-        short myshort=2334;
-        int myint=123445566;
-        long mylong=(long)((mybyte+myshort+myint)*10)+5000;
-        System.out.println("My long ="+mylong);
+        //E means to the power of or the ^
+        //the range of data a float can store
+        float myminfloat=Float.MIN_VALUE;
+        float mymaxfloat=Float.MAX_VALUE;
+        System.out.println("the min data a float can handle is "+myminfloat);
+        System.out.println("the max amount of data a float can handle is "+mymaxfloat);
+
+        //the range of data a double can store
+        double mymindouble=Double.MIN_VALUE;
+        double mymaxdouble=Double.MAX_VALUE;
+        System.out.println("the min data a double can handle is "+mymindouble);
+        System.out.println("the max amount of data a double can handle is "+mymaxdouble);
+        //float value ends in f
+        float myNewfloatvalue=14.5f;
+        // double value ends in d
+        double myNewdoublevalue=14.5d;
+        int myintvalue=14;
     }
 }
