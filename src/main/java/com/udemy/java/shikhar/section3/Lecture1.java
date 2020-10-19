3141aadc615a7023af61482baf8e6be304c43687
schpoof2u <62222006+schpoof2u@users.noreply.github.com>
2020-10-18 22:17:57 -0400

    3.16 Operators,Operands and Expressions learned about the shortcuts for certain operators and how to use a shortcut to use and reassign the value of a variable using an operator

--- src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
+++ src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
@@ -3,30 +3,40 @@ package com.udemy.java.shikhar.section3;
 public class Lecture1 {
     public static void main(String[] args)
     {
-        // addition operator or +
-        // + does addition
-        int myresult=12+8; // 12+6=18
-        System.out.println("12+6= "+myresult);
-        int previousresult=myresult;
-        System.out.println("the previous result is "+previousresult);
-        int newresult= myresult-1;
-        System.out.println("the new result is 12+5-1= "+ newresult);
+        byte mycurrentage=14;
+        System.out.println("my age in 2020 is "+mycurrentage);
+        // ++ means adding 1 to the value and reassgining it currentage can be written as currentage=currentage+1 or currentage++
+        mycurrentage++;
+        System.out.println("my age in 2021 is "+mycurrentage);
 
-        //multiplication operator or *
-        // * does multiplication
-        myresult=myresult*2;
-        System.out.println("18*2= "+myresult);
+        byte numofcakes=12;
+        System.out.println("their are "+numofcakes+" cakes. Johnny takes 1. \n how many do you have left?");
+        // is the subtracting form of ++
+        numofcakes--;
+        System.out.println("you have "+numofcakes+" cakes left");
 
-        //division operator or /
-        myresult=12+8;
-        // / does division
-        myresult=myresult/5;
-        System.out.println("20/5= "+myresult);
+        byte breadloaves=11;
+        System.out.println("you have "+breadloaves+" loaves of bread at home");
+        // adding a value by an amount greater than 1 and reassigning it
+        breadloaves+=3;
+        System.out.println("you go out and buy 3 more loaves of bread.\n you now have "+breadloaves+" loaves of bread");
 
-        //the remainder/modular operator or %
-        // % calculates/returns the remainder of a operand dived by another operand
-        myresult=17%3;
-        System.out.println("the remainder of 17/3= "+myresult);
+        byte cookiejar=100;
+        System.out.println("their are "+cookiejar+" cookies in a cookie jar");
+        //subtracting a value by an amount greater than 1 and reassigning it
+        cookiejar-=4;
+        System.out.println("you take 4 cookies out of the cookie jar \n their are now "+cookiejar+" cookies in the cookie jar");
 
+        byte tenten=10;
+        System.out.println("what is "+tenten+" squared?");
+        //mutiplying a value by an amount greater than 1 and reassigning it
+        tenten*=10;
+        System.out.println("10 squared is "+tenten);
+
+        byte six=6;
+        System.out.println("what is "+six+" divided by 2");
+        //dividing a value by an amount greater than 1 and reassigning it
+        six/=2;
+        System.out.println("6 divided by 2 is "+six);
     }
 }
