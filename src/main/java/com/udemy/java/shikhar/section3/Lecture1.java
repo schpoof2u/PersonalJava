cb5fb869ee66be37ae631d06277b65e143f15059
schpoof2u <62222006+schpoof2u@users.noreply.github.com>
2020-10-24 22:05:38 -0400

    3.19 Logical and Operator learned about the and operator in java also the previous 3.19 is actually 3.18

--- src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
+++ src/main/java/com/udemy/java/shikhar/section3/Lecture1.java
@@ -11,5 +11,14 @@ public class Lecture1 {
            System.out.println("User is not Daksh.");
            System.out.println("this is good because Daksh likes  marshmallow pie");
        }
+        int topScore=100;
+        //<= means less than or equal too, => greater than or equal to
+        if(topScore<=100) {
+            System.out.println("My high score in pac man is "+topScore);
+        }
+        int secondTopScore=50;
+        if (topScore>secondTopScore && topScore<150){
+            System.out.println("The top score is greater than the second score and is less than 150");
+        }
     }
 }
