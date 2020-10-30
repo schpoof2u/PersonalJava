67e9978ab1ffa09888ea97155335ebe8c9ea7781
schpoof2u <62222006+schpoof2u@users.noreply.github.com>
2020-10-29 21:11:27 -0400

    experiment java class to test new thing learned

---
+++ src/main/java/com/selflearn/java/MyExperiments.java
@@ -0,0 +1,65 @@
+package com.selflearn.java;
+
+public class MyExperiments {
+    public static void main(String[] args) {
+//        short x = 32767;
+//        short s = (short) (x / 10);
+
+//        byte b = 10;
+//        short s = 20;
+//        int i = 50;
+//        long l = 50000 + 10 * (b + s + i);
+//
+//        short k=Short.MAX_VALUE;
+//        short m=Short.MIN_VALUE;
+//        System.out.println(k);
+//        System.out.println(m);
+//
+//        float f = 5 / 3f;
+//        double d = 5d / 3;
+//        System.out.println(f);
+//        System.out.println(d);
+//
+//        int i = 1;
+//        System.out.println("i = " + i++);
+//        System.out.println("i = " + ++i);
+//        i++;
+//        System.out.println("i = " + i);
+//        System.out.println("i = " + i++);
+//        ++i;
+//        System.out.println("i = " + i);
+//
+//        boolean isAlien = true;
+//        if (isAlien) {
+//            System.out.println("this is an alien");
+//        }
+//        if (!isAlien) {
+//            System.out.println("this is not an alien");
+//        }
+//
+//        int i = 10;
+//        int j = 20;
+//        int k = 30;
+//        boolean cond1 = (i > 9 && j > 11);
+//        boolean cond2 = i > 9 && j > 21 || k < 30;
+//        System.out.println(cond2);
+//
+//        int i = 50;
+//        if (i = 50) {
+//            System.out.println("why compile error");
+//        }
+//        boolean j = false;
+//        if (j = true) {
+//            System.out.println("why this worked");
+//        }
+//
+//        int i = 10;
+//        int j = (i > 10) ? 20 : 100;
+//        if (i > 10) {
+//            j = 20;
+//        } else {
+//            j = 100;
+//        }
+//        int j = (i > 10) ? (i < 6 ? 20 : 50) : 100;
+    }
+}
