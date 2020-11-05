15ff890abb6beac6b9b76e13a64869a25e783d18
schpoof2u <62222006+schpoof2u@users.noreply.github.com>
2020-11-05 18:15:18 -0500

    Lecture 4.1 learned about java keywords

---
+++ src/main/java/com/udemyjava/shikhar/section4/Lecture1.java
@@ -0,0 +1,12 @@
+package com.udemyjava.shikhar.section4;
+
+public class Lecture1 {
+    public static void main(String[]args){
+        //Java has 50 reserved keywords that hold a specific function in java
+        // int is a java keyword
+        //any highlighted/colored word(orange in dark mode, blue in light mode) is a keyword
+        int myAge=14;
+        String javaKeyword=("What is a java keyword? \n A java keyword is a word that has been taken by java, that holds a specific purpose \n  for example, int is a keyword \n any highlighted/colored word(orange in dark mode, blue in light mode) is a keyword");
+        System.out.println(javaKeyword);
+    }
+}
