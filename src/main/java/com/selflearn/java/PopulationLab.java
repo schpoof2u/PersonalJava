7c03003bd50a94d7646753094d6f07f8383d6817
schpoof2u <62222006+schpoof2u@users.noreply.github.com>
2020-12-24 17:57:25 -0500

    This a program I wrote to help me do my Population lab for science

---
+++ src/main/java/com/selflearn/java/PopulationLab.java
@@ -0,0 +1,7 @@
+package com.selflearn.java;
+
+public class PopulationLab {
+    public static void main(String[]args){
+        System.out.println("This a program I wrote to help me do my Population lab for science");
+    }
+}
