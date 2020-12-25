9523a968268ced9079994e4bce4f72e76c2f9108
schpoof2u <62222006+schpoof2u@users.noreply.github.com>
2020-12-25 00:19:43 -0500

    made a variable for the total amount of beans

--- src/main/java/com/selflearn/java/PopulationLab.java
+++ src/main/java/com/selflearn/java/PopulationLab.java
@@ -1,7 +1,22 @@
 package com.selflearn.java;
 
 public class PopulationLab {
+    //Start with 100 paperclips/beans/popcorn(or anything else you can find).
+    //Count out 5 beans to represent the beginning of the population.
+    //Find 20% of the population, round to the nearest whole number.
+    //Every year 20% of the population have two offspring.
+    //Record data in chart.
+    //Add the correct amount of beans to your population.
+    //Every year 20% of the population dies.
+    //Record data in chart.
+    //Subtract the correct amount of beans from your population,
+    //Count the remaining beans in your population.
+    //Record data in chart.
+    //Continue for 20 years.
     public static void main(String[]args){
         System.out.println("This a program I wrote to help me do my Population lab for science");
+    }
+    public static void calcPopulation(){
+        int beanNum=100;
     }
 }
