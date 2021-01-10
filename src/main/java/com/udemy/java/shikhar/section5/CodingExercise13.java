3045957843f0b6e4892e00a0adb36dc5d056a28e
schpoof2u <62222006+schpoof2u@users.noreply.github.com>
2021-01-09 22:06:36 -0500

    CodingExercise13 works and is about seeing how many days are within a certain year

--- src/main/java/com/udemy/java/shikhar/section5/CodingExercise13.java
+++ src/main/java/com/udemy/java/shikhar/section5/CodingExercise13.java
@@ -44,7 +44,7 @@ public class CodingExercise13 {
     }
 
     public static int getDaysInMonth(int month, int year) {
-        if ((month < 1 && month > 12) && (year < 1 && year > 9999)) {
+        if ((month < 1 || month > 12) || (year < 1 || year > 9999)) {
             return -1;
         }
         if(isLeapYear(year)==true){
@@ -53,28 +53,13 @@ public class CodingExercise13 {
             }
             else{
                 switch(month){
-                    case 1:
+                    case 1: case 3: case 5: case 7: case 8:case 10:case 12:
                         return 31;
-                    case 3:
-                        return 31;
-                    case 4:
+                    case 2:
+                        return 28;
+                    case 4: case 6:case 9:case 11:
                         return 30;
-                    case 5:
-                        return 31;
-                    case 6:
-                        return 30;
-                    case 7:
-                        return 31;
-                    case 8:
-                        return 30;
-                    case 9:
-                        return 31;
-                    case 10:
-                        return 30;
-                    case 11:
-                        return 31;
-                    case 12:
-                        return 30;
+
                     default:
                         return -1;
                 }
@@ -82,33 +67,15 @@ public class CodingExercise13 {
         }
         else{
             switch(month){
-                case 1:
+                case 1: case 3: case 5: case 7: case 8:case 10: case 12:
                     return 31;
                 case 2:
                     return 28;
-                case 3:
-                    return 31;
-                case 4:
+                case 4: case 6:case 9:case 11:
                     return 30;
-                case 5:
-                    return 31;
-                case 6:
-                    return 30;
-                case 7:
-                    return 31;
-                case 8:
-                    return 31;
-                case 9:
-                    return 30;
-                case 10:
-                    return 31;
-                case 11:
-                    return 30;
-                case 12:
-                    return 31;
                 default:
                     return -1;
             }
         }
-        }
     }
+}
