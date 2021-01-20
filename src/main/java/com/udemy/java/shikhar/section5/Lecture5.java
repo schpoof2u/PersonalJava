195c759027f968cd837f35c1a17d80b16b7d5cca
schpoof2u <62222006+schpoof2u@users.noreply.github.com>
2021-01-19 22:37:09 -0500

    try 1 of CodingExercise14

--- src/main/java/com/udemy/java/shikhar/section5/Lecture5.java
+++ src/main/java/com/udemy/java/shikhar/section5/Lecture5.java
@@ -4,6 +4,18 @@ public class Lecture5 {
     public static void main(String[]args) {
         int count = 0;
         int sum = 0;
+        for (int i = 1; i <= 1000; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                count++;
+                sum += i;//Remember += stores an instance of i and then adds the next value of i to the previous value of i
+                System.out.println(i);
+            }
+            if (count == 5) {
+                break;
+            }
+        }
+        System.out.println(sum);
+    }
 //        for (int i = 1; i <= 1000; i++) {
 //            if ((i % 3 == 0) && (i % 5 == 0)) {
 //                count++;
@@ -14,16 +26,5 @@ public class Lecture5 {
 //                break;
 //            }
 //        }
-            for (int i = 1; i <= 1000; i++) {
-                if ((i % 3 == 0) && (i % 5 == 0)) {
-                    count++;
-                    sum += i;//Remember += stores an instance of i and then adds the next value of i to the previous value of i
-                    System.out.println(i);
-                }
-                if (count == 5) {
-                    break;
-                }
-            }
-        System.out.println(sum);
-        }
+
     }
