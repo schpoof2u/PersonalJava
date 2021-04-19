b28acc7616391a30d42d11ca382aa2c838a9ab10
schpoof2u <62222006+schpoof2u@users.noreply.github.com>
2021-04-18 20:52:19 -0400

    finished part 1

--- src/main/java/com/udemy/java/shikhar/section5/CodingExercise25.java
+++ src/main/java/com/udemy/java/shikhar/section5/CodingExercise25.java
@@ -28,49 +28,53 @@ public class CodingExercise25 {
     //NOTE: The method getLargestPrime should be defined as public static like we have been doing so far in the course.
     //
     //NOTE: Do not add a main method to the solution code.
-    public static void main(String[]args){}
-    public static long getLargestPrime(int number){
-        if(number<=0 || number==1){
-            return -1;
-        }
-        else{
-            long squareRoot=Math.round(Math.sqrt(number));//max limit of factors
-            long primeFactor=0;
-            int count=0;
+    public static void main(String[] args) {
+//        System.out.println(getLargestPrime(21));//should return 7 but instead returns -1
+//        System.out.println(getLargestPrime(25));//should return 5 but instead returns -1
+//        System.out.println(getLargestPrime(31));//verifies that the loop that checks if number is a prime number works
+//        System.out.println(getLargestPrime(79));
+        //////////////////////////////////////////////////////////////
 
-            for(int k=1;k<=number;k++){//checks to see if the number is a prime number
-                if(number%k==0){
+        //*start section, gets the factors of a number
+        int number=27;
+        long squareCounter=0;
+        int c=0;
+        long squareRoot=Math.round(Math.sqrt(number));
+        for (int i=1;i<=number;i++){
+            if(number%i==0){
+                c++;
+
+                System.out.println(number/i);
+                squareCounter++;
+            }
+            if(squareCounter==squareRoot){
+                break;
+            }
+        }
+        System.out.println(c);
+        //* end section
+    }
+
+    public static int getLargestPrime(int number) {
+        if (number <= 1) {
+            return -1;
+        } else {
+            int count = 0;
+            for (int k = 1; k <= number; k++) {//checks to see if the number is a prime number
+                if (number % k == 0) {
                     count++;
                 }
             }
-            if(count==2){
+            if (count == 2) {
                 return number;
             }
 
             /////////////////////////////////////////////////////
+            count = 0;
 
-            for(int i=2;i<=squareRoot;i++){// is the loop that divides number/i
-                if(i>number/i){//checks to see if i is greater then number/i
-                    for(int o=1;o<=i;o++){//divides i by o
-                        if(i%o==0){
-                            count++;
-                        }
-                    }
-                    if(count==2){// if count equals 2 that means the number is prime since it is only divisible by itself and 1
-                        primeFactor=i;
-                    }
-                }
-                else if(i<number/i){
-                    for(int o=1;o<=(number/i);o++){
-                        if((number/i)%o==0){
-                            count++;
-                        }
-                    }
-                    if(count==2){
-                        primeFactor=(number/i);
-                    }
-                }
-            }
+
+            //Logic:
+            // run a for loop that divides  number by i to print out its factors
         }
         return -1;
     }
