f8ccd630b3d36a14ebf6fb51315e28b424c1368a
Shikhar <62222006+schpoof2u@users.noreply.github.com>
2021-07-18 13:14:44 -0400

    implemented a character checker(Lecture 10)

--- src/main/java/com/udemy/java/shikhar/section5/Lecture10.java
+++ src/main/java/com/udemy/java/shikhar/section5/Lecture10.java
@@ -10,21 +10,24 @@ public class Lecture10 {
         System.out.println("Enter your name: ");
         String name=scanner.nextLine();//returns input that the user typed in the console, scans for a string
         System.out.println("Your name is "+name);//displays what what returned
-        System.out.println();
 
         //asks user for their year of birth
         System.out.println("Enter your year of birth:");
         int yearOfBirth= scanner.nextInt();
         System.out.println("Your year of birth is "+yearOfBirth);//scans for an int
-        scanner.close();//closes scanner as to release the memory used by scanner
+        //scanner.close();//closes scanner as to release the memory used by scanner
         int age=2021-yearOfBirth;
-        System.out.println();
+        //Line below is a solution for when taking integer/primitive type input
+        scanner.nextLine();//handles the next line issue,user presses enter key and line ends
         System.out.println(name+" you are "+age+" years old");
-//        System.out.println("Enter a character on your keyboard");
-//        String character=scanner.nextLine();
-//        if(character.length()>1){
-//            System.out.println("Invalid");
-//        }
-//        System.out.println(character);
+        System.out.println("Enter a character on your keyboard:");
+        String character=scanner.nextLine();
+        if(character.length()>1){
+            System.out.println("Invalid");
+        }
+        else{
+            System.out.println("You entered "+character);
+        }
+        scanner.close();
     }
 }
