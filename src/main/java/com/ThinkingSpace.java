3cf8a080fff45e6a4b7224b8571b9770879dc8fa
Shikhar <62222006+schpoof2u@users.noreply.github.com>
2021-10-30 18:25:44 -0400

    Remembering how to parse

--- src/main/java/com/ThinkingSpace.java
+++ src/main/java/com/ThinkingSpace.java
@@ -2,35 +2,14 @@ package com;
 import java.util.Scanner;
 public class ThinkingSpace {
     public static void main(String[]args){
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("What is your name:");
-        String name=scanner.nextLine();
-        System.out.println("your name is "+name);
-
-        System.out.println("What is the current year:");
-        boolean hasNextInt=  scanner.hasNextInt();
-        int currentYear= scanner.nextInt();
-        if(hasNextInt) {
-            System.out.println("the year is " + currentYear);
-        }
-        else{
-            System.out.println("Unable to parse currentYear");
-        }
-        System.out.println("What is your birth year:");
-        boolean hasNextInt2=scanner.hasNextInt();
-        int birthYear=scanner.nextInt();
-        if(birthYear<0 || birthYear>currentYear || !hasNextInt2){
-            System.out.println("Invalid");
-        }
-        else {
-            System.out.println("your birth year is " + birthYear);
-        }
-        int age=currentYear-birthYear;
-        System.out.println();
-        System.out.println("User Profile ("+currentYear+")");
-        System.out.println("Name: "+name);
-        System.out.println("Birth Year: "+birthYear);
-        System.out.println("Age: "+age);
-
+       String numbers="1234";
+       String numbers2="23f";
+       String numbers3="123";
+       long number=Long.parseLong(numbers);
+       int number2=Integer.parseInt(numbers);
+       double number3=Double.parseDouble(numbers);
+       float number4=Float.parseFloat(numbers2);
+       byte number5=Byte.parseByte(numbers3);
+        System.out.println(number5);
     }
 }
